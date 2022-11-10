4bad92327e4c03b29063777f55cc7cb13be84fec
carloseduneves <carlosneves.edu@gmail.com>
2022-11-10 16:33:44 -0300

    juros-comp

--- src/App.js
+++ src/App.js
@@ -1,17 +1,25 @@
 
+//components react
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
+//states
 import {useState} from 'react';
-import Header from './components/Header';
+//icons to cards
 import casa from './icons/casa.png';
 import carroNovo from './icons/carro-novo.png';
 import money from './icons/money.png';
-//import Spinner from 'react-bootstrap/Spinner';
 //components
 import JurosCarro from './components/JurosCarro.js';
 import JurosCasa from './components/JurosCasa.js';
 import JurosInvest from './components/JurosInvest.js';
 
+//header components
+import Container from 'react-bootstrap/Container';
+import Form from 'react-bootstrap/Form';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+//import NavDropdown from 'react-bootstrap/NavDropdown';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
 //control options pages
@@ -58,23 +66,53 @@ const [formulary, setFormulary] = useState(false);
         console.log(allInterest);
         console.log(calculate);
         console.log(totalInterest);
+ 
         return;
+    }
+  //return to home
+  const returnToHome = () => {
+    setPage(stages[0].name);
+    setFormulary(false);
     }
 
   return (
     <div className="App">
       <main>
         <header>
-          <Header/>
+          <Navbar bg="light" expand="lg">
+              <Container fluid>
+              <Navbar.Brand onClick={returnToHome}><h4 style={{cursor: 'pointer'}}>Página inicial</h4></Navbar.Brand>
+              <Navbar.Toggle aria-controls="navbarScroll" />
+              <Navbar.Collapse id="navbarScroll">
+              <Nav
+              className="me-auto my-2 my-lg-0"
+              style={{ maxHeight: '100px' }}
+              navbarScroll>
+
+              <Nav.Link onClick={returnToHome}>Home</Nav.Link>
+              <Nav.Link onClick={showHouse}>Calcurar juros sobre imóvel</Nav.Link>
+
+              
+                  <Nav.Link href="#" disabled>
+                      Link
+                  </Nav.Link>
+                  </Nav>
+                  <Form className="d-flex">
+                    <Form.Control
+                      type="search"
+                      placeholder="Search"
+                      className="me-2"
+                      aria-label="Search"/>
+                      <Button variant="outline-success">Search</Button>
+                  </Form>
+                </Navbar.Collapse>
+              </Container>
+            </Navbar>
         </header>
         <article>
 
         </article>
         {page === 'start' && <div >
-            
-             
-
-            
             <p>Para começar, escolha uma das opções de investimento abaixo:</p>
             <div className='cards'>
               <div>
@@ -115,7 +153,12 @@ const [formulary, setFormulary] = useState(false);
                </div>
             </div>
           </div>}
-        {formulary === true && <form className='form ' onSubmit={calc}>
+          <div className='results'>
+          {page === 'car' && calcInterest > 0 &&(<JurosCarro calcInterest={calcInterest} allInterest={allInterest} />)}
+          {page === 'house' && calcInterest > 0 && (<JurosCasa calcInterest={calcInterest} allInterest={allInterest} />)}
+          {page === 'money' && calcInterest > 0 && (<JurosInvest calcInterest={calcInterest} allInterest={allInterest} />)}
+        </div>
+        {formulary === true &&  <form className='form ' onSubmit={calc}>
         {page === 'house' && <h1>Investimento imobiliário</h1>}
               {page === 'car' && <h1>Investimento em automóveis</h1>}
               {page === 'money' && <h1>Investimento Financeiro</h1>}
@@ -124,13 +167,9 @@ const [formulary, setFormulary] = useState(false);
             <input type="text" name="tempo" placeholder="tempo para pagar" onChange={(e) => setTime(e.target.value)}/>
             <input type="text" name="valor" placeholder="taxa de juros a.m." onChange={(e) => setInterest(e.target.value)}/>
             <button className="btnForm" >calcular</button>
-            
-
         </form>}
-        {page === 'car' && calcInterest > 0 &&(<JurosCarro calcInterest={calcInterest} allInterest={allInterest} />)}
-        {page === 'house' && calcInterest > 0 && (<JurosCasa calcInterest={calcInterest} allInterest={allInterest} />)}
-        {page === 'money' && calcInterest > 0 && (<JurosInvest calcInterest={calcInterest} allInterest={allInterest} />)}
-       
+
+        
       </main>
     </div>
   );
